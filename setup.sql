498cae58dcae199da5ec2a67f84259a051e44d65
gwrobbins1 <gwrobbins1@gmail.com>
2017-04-03 22:58:59 -0400

    adding setup sql script

---
+++ setup.sql
@@ -0,0 +1,18 @@
+CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
+USE `mydb` ;
+
+CREATE TABLE IF NOT EXISTS `mydb`.`Sensors` (
+  `SID` INT NOT NULL,
+  `Latitude` DOUBLE NULL,
+  `Longitude` DOUBLE NULL,
+  PRIMARY KEY (`SID`))
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `mydb`.`Recorded_Data` (
+  `Index` INT NOT NULL AUTO_INCREMENT,
+  `SID` INT NULL,
+  `TIME` DATETIME NULL,
+  `Completed` TINYINT(1) NOT NULL,
+  `Frequency` DOUBLE NULL,
+  `Readings` DOUBLE NULL,
+  PRIMARY KEY (`Index`));
